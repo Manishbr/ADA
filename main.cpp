7b75bf9ce68f276b23fdad6067b4607564a9dd8b
Manishbr <51982464+Manishbr@users.noreply.github.com>
2019-08-14 13:27:55 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,29 +1,36 @@
-#include<stdio.h>
-
-
-
+#include <iostream>
+using namespace std;
 int main()
 {
-	int i,n=0,max=0,arr[10];
-	printf("print the number of integers");
-	scanf("%d",&n);
-	printf("enter the values");
-	for(i=0;i<n;i++)
+	int num,start,end,sq,mid;
+	cout <<"enter the number";
+	cin >>num;
+	start=0;
+	end=num=-1;
+	mid=(start+end)/2;
+	while(start<=end)
 	{
-		scanf("%d",&arr[i]);
-
-	}
-	max=arr[i];
-	for(i=0;i<n;i++)
-	{
-		if(max<arr[i])
-		{
-			max=arr[i];
+		if(mid*mid==num){
+			sq=mid;
+			break;
+		}
+		else if (mid*mid>num){
+			end=mid-1;
 			
 		}
+		else {
+			start=mid+1;
+		     sq=mid;
+			 
+			}
+			mid=(start+end)/2;
 	}
-	printf("/n maximum is %d",max);
-	return 0;
+	cout<<sq;
 	
+	
+	
+	
+	}
 
-}
+
+
